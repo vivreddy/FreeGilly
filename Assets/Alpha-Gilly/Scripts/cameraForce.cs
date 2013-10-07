f8770e7b60bc266e3d5b55e5a9073e3bb3fc2e78
vivek reddy <vivreddy@seas.upenn.edu>
2013-10-06 17:10:18 -0700

    Base commit.

---
+++ Assets/Alpha-Gilly/Scripts/cameraForce.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraForce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	gameObject.rigidbody.AddForce(new Vector3(0,0,10));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.rigidbody.AddForce(new Vector3(0,0,10));
+	}
+}
